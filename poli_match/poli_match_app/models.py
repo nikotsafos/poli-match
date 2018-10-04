055c195027388d559d53585319af63c077f215dd
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-10-04 15:54:13 -0700

    Setting up the models

--- poli_match/poli_match_app/models.py
+++ poli_match/poli_match_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+
+    def __str__(self):
+        return self.first_name
+
+class Politician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+class Quote(models.Model):
+    content = models.TextField()
+    politician = models.ForeignKey(Politician, on_delete=models.CASCADE, related_name='quotes')
+
+    def __str__(self):
+        return self.content
