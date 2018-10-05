df2ee6c65420859fce24baa0c244ae531537f742
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-10-04 18:07:24 -0700

    Login and Signup working

--- poli_match/poli_match_app/urls.py
+++ poli_match/poli_match_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
     path('login', views.login, name="login"),
     path('signup', views.signup, name="signup"),
 ]
