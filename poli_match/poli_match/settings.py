5e9d33123499dd1a6d2b26f449a10b2338c08d65
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-10-04 17:10:07 -0700

    Made politician and quote models and migrate

--- poli_match/poli_match/settings.py
+++ poli_match/poli_match/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'poli_match',
+    'poli_match_app',
 ]
 
 MIDDLEWARE = [
--- poli_match/poli_match_app/models.py
+++ poli_match/poli_match_app/models.py
@@ -1,24 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    photo_url = models.TextField()
-
-
-    def __str__(self):
-        return self.first_name
 
 class Politician(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     photo_url = models.TextField()
+    summary = models.TextField()
 
 
     def __str__(self):
-        return self.first_name + self.last_name
+        return self.last_name
 
 class Quote(models.Model):
     content = models.TextField()
