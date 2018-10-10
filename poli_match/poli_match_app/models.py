96572e8dadfb39624e7249bcf8a2c425e9c1f9af
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-10-10 12:20:02 -0700

    Fixing models

--- poli_match/poli_match_app/models.py
+++ poli_match/poli_match_app/models.py
@@ -10,20 +10,21 @@ class Politician(models.Model):
     gender = models.CharField(max_length=10, default=True)
     party = models.CharField(max_length=10)
     twitter_account = models.CharField(max_length=25, null=True)
-    url = models.CharField(max_length=50, default=True)
+    url = models.CharField(max_length=100, default=True)
     total_votes = models.IntegerField(default=0)
-    missed_votes = models.IntegerField(default=0)
+    missed_votes = models.IntegerField(default=0, null=True)
     phone = models.CharField(max_length=25, default=True, null=True)
     state = models.CharField(max_length=10, default=True)
-    state_rank = models.CharField(max_length=20, default=True)
-    missed_votes_pct = models.IntegerField(default=0)
-    votes_with_party_pct = models.IntegerField(default=0)
+    state_rank = models.CharField(max_length=101, default=True, null=True)
+    missed_votes_pct = models.FloatField(default=0)
+    votes_with_party_pct = models.FloatField(default=0)
 
     def __str__(self):
         return self.last_name
 
 class Quote(models.Model):
     content = models.TextField()
+    source = models.CharField(max_length=50, default='some string')
     politician = models.ForeignKey(Politician, on_delete=models.CASCADE, related_name='quotes')
 
     def __str__(self):
